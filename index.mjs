90995de773f84326b2f38e14eb46198e441f0824
Bernhard Lukassen <bl@bernhard-lukassen.com>
2021-06-16 13:08:47 +0200

    refactored loaders, prepare for distributed repositories, fixes

--- index.mjs
+++ index.mjs
@@ -9,6 +9,7 @@ import Controller                           from "./lib/controller/controller.mj
 // export { default as LocationWatcher }       from './lib/controller/locationwatcher.mjs';
 export { default as ComponentsWatcher }     from './lib/controller/componentswatcher.mjs';
 export { default as ComponentDescriptor }   from './lib/component/componentcontext.mjs';
+export { ComponentContext }                 from './lib/component/componentcontext.mjs';
 
 // mockup for a service component; implement all methods in your class, it will be recognised automatically
 export { default as ServiceComponent }      from './lib/component/servicecomponent.mjs';
